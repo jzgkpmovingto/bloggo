a83a01396fd879f8893eb82246b0d31496807345
jzgkpmovingto <jzgkpmoving@gmail.com>
2019-08-03 16:25:25 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,17 @@
 @section('title','主页')
 
 @section('content')
-	<h1>主页</h1>
+	<div class="jumbotron">
+		<h1>Hello kidder</h1>
+		<p class="lead">
+			你现在看到的是 <a href="https://www.baidu.com">baidu
+			</a>的入口
+		</p>
+		<p>
+			一切将从这里开始
+		</p>
+		<p>
+			<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+		</p>
+	</div>
 @stop
